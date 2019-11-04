b30be087d84f48c25b4d0f68304d0aefedd34181
Will Hodgson <will.hodgson@current-rms.com>
2019-11-04 13:55:32 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-  # 
+  #
   # def genre_id=(name)
   #   genre = Genre.find_or_create_by(name: name)
   #   self.genre = genre
@@ -20,5 +20,17 @@ class Song < ActiveRecord::Base
 
   def artist_name
     self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(notes)
+    notes.each do |content|
+      if content.strip != ''
+        self.notes.build(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
   end
 end
