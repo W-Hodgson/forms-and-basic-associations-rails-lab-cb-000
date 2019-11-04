4f6050f6118a256c81930c27b658fcec15395ed6
Will Hodgson <will.hodgson@current-rms.com>
2019-11-04 13:55:01 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,15 +3,15 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-
-  def genre_id=(name)
-    genre = Genre.find_or_create_by(name: name)
-    self.genre = genre
-  end
-
-  def genre_name
-    self.genre ? self.genre.name : nil
-  end
+  # 
+  # def genre_id=(name)
+  #   genre = Genre.find_or_create_by(name: name)
+  #   self.genre = genre
+  # end
+  #
+  # def genre_name
+  #   self.genre ? self.genre.name : nil
+  # end
 
   def artist_name=(name)
     artist = Artist.find_or_create_by(name: name)
